ebb7f2667be642b95e0ef6212b560f76f251dc8c
tsenkwong <67427208+tsenkwong@users.noreply.github.com>
2025-01-14 11:40:55 +0100

    Feature/pi 748 split build process (#52)
    * prepare split for possible portal
    
    * update environments
    
    * fix frontend build
    
    * add missing nginx config
    
    * ignore cors for now
    
    * update readme
    
    * Fix Pipeline
    
    * Fix Pipeline
    
    * Fix Pipeline
    
    * Add Split Pipeline
    
    * Remove from feature Pipeline
    
    * remove cors annotation
    
    * fix frontend test
    
    * remove feature branch from ci
    
    ---------
    
    Co-authored-by: M-Busk <125284318+M-Busk@users.noreply.github.com>
    Co-authored-by: Tsen Kwong <tsen.kwong.ext@hermesworld.com>

--- frontend/src/app/interceptors/auth.interceptor.spec.ts
+++ frontend/src/app/interceptors/auth.interceptor.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthInterceptor } from './auth.interceptor';
+import {AuthInterceptor} from './auth.interceptor';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AuthService', () => {
   let service: AuthInterceptor;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthInterceptor]
+    });
     service = TestBed.inject(AuthInterceptor);
   });
 
