e847cd6abdb75cd1ce9be8ed8cd50c52aeb3a815
cengiz-yazici <cengiz.yazici@capgemini.com>
2025-01-16 10:24:01 +0100

    adjusted frontend test for login component

--- frontend/src/app/views/login/login.component.spec.ts
+++ frontend/src/app/views/login/login.component.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {HomeComponent} from "../home/home.component";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
+    await TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule]
+    })
+      .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
