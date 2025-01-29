c7f5e05a9cfaa188006d8725b66061620deb747d
cengiz-yazici <cengiz.yazici@capgemini.com>
2025-01-29 17:08:34 +0100

    added logout in case of false credentials

--- frontend/src/app/services/mgmt/auth/auth-resolver.service.ts
+++ frontend/src/app/services/mgmt/auth/auth-resolver.service.ts
@@ -1,11 +1,13 @@
 import {ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot} from '@angular/router';
 import {ApiService} from "../api/api.service";
 import {inject} from "@angular/core";
+import {AuthService} from "./auth.service";
 
 export const isAuthenticated: ResolveFn<any> =
   async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> => {
     const router = inject(Router);
     const apiService = inject(ApiService);
+    const authService = inject(AuthService);
 
     try {
       const response = await apiService.getAllRegistrationRequests();
@@ -13,6 +15,7 @@ export const isAuthenticated: ResolveFn<any> =
     } catch (e) {
       console.log(e);
       if (e.status === 401 || e.status === 403) {
+        authService.logout();
         switch (e.status) {
           case 401:
             alert(`Invalid Credentials used. Please log in again.`);
